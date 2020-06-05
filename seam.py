adecdb4b4a6a92b9f49f166e163b94ead36d2ec1
Davis Ranney <bowtie@davisranney.com>
2020-06-04 23:12:05 -0400

    added comments to seam

--- seam.py
+++ seam.py
@@ -6,43 +6,43 @@ def carve_seams(frame):
 	energies = np.zeros(frame.shape[1])
 
 	min_energy = np.amin(frame[0, 0:2])
-	y = np.where(frame[0, 0:2] == min_energy)[0][0]
+	y = np.where(frame[0, 0:2] == min_energy)[0][0] # calculates seam for top left corner
 	seams[0].append([0, y]) 
 	seams[0].append([1, 0])
 	energies[0] += min_energy + frame[1][0] 
 
 	for j in range(1, frame.shape[1] - 1):
 		min_energy = np.amin(frame[0, j-1:j+2])
-		y = j - 1 + np.where(frame[0, j-1:j+2] == min_energy)[0][0]
+		y = j - 1 + np.where(frame[0, j-1:j+2] == min_energy)[0][0] #calculates initial seams for the second row, from the first row
 		seams[j].append([0, y])
 		seams[j].append([1, j]) 
 		energies[j] += min_energy + frame[1][j] 
 
 	min_energy = np.amin(frame[0, frame.shape[1]-2:frame.shape[1]])
-	y = frame.shape[1] - 2 + np.where(frame[0, frame.shape[1]-2:frame.shape[1]] == min_energy)[0][0]
+	y = frame.shape[1] - 2 + np.where(frame[0, frame.shape[1]-2:frame.shape[1]] == min_energy)[0][0] #calculates seam from top left
 	seams[frame.shape[1]-1].append([0, y])
 	seams[frame.shape[1]-1].append([1, frame.shape[1]-1]) 
 	energies[frame.shape[1]-1] += min_energy + frame[1][frame.shape[1]- 1] 
 		
-	for i in range(2, frame.shape[0]):
+	for i in range(2, frame.shape[0]): #loops through all subsequent rows
 		new_seams = [[] for i in range(frame.shape[1])]
 		new_energies = np.zeros(frame.shape[1])
 
 		min_energy = np.amin(energies[0:2])
-		y = np.where(energies[0:2] == min_energy)[0][0]
+		y = np.where(energies[0:2] == min_energy)[0][0] #calculates the new seam to the left
 		new_seams[0] = seams[y].copy()
 		new_seams[0].append([i, 0]) 
 		new_energies[0] = min_energy + frame[i][0] 
 
 		for j in range(1, frame.shape[1] - 1):
 			min_energy = np.amin(energies[j-1:j+2])
-			y = j - 1 + np.where(energies[j-1:j+2] == min_energy)[0][0]
+			y = j - 1 + np.where(energies[j-1:j+2] == min_energy)[0][0] #calculates all middle seams
 			new_seams[j] = seams[y].copy()
 			new_seams[j].append([i, j])  
 			new_energies[j] = min_energy + frame[i][j]
 
 		min_energy = np.amin(energies[frame.shape[1]-2:frame.shape[1]])
-		y = frame.shape[1] - 2 + np.where(energies[frame.shape[1]-2:frame.shape[1]] == min_energy)[0][0]
+		y = frame.shape[1] - 2 + np.where(energies[frame.shape[1]-2:frame.shape[1]] == min_energy)[0][0] #calculates last seam of a row
 		new_seams[frame.shape[1]-1] = seams[y].copy()
 		new_seams.append([i, frame.shape[1]-1]) 
 		new_energies[frame.shape[1]-1] = min_energy + frame[i][frame.shape[1]- 1] 
