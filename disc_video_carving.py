3df74db0f09a395c841d40504b832822b8042f27
DovydasAL <davidlazauskas1@gmail.com>
2020-06-05 00:16:48 -0400

    modify and add new existing impl of TC

--- disc_video_carving.py
+++ disc_video_carving.py
@@ -2,6 +2,7 @@ import cv2
 import numpy as np
 import argparse
 import imageio as img
+import seam
 
 # ------------Things to Implement-------------------
 
@@ -99,25 +100,37 @@ def saliency_map(frame):
 # currentFrame = numpy array
 # previousSeam = numpy array of pairs of points
 # x, y = point to compute in the current frame
-def compute_temporal_coherence_cost_pixel(currentFrame, previousSeam, x, y):
-    seamX = previousSeam[y][0]
+def compute_temporal_coherence_cost_pixel1(currentFrame, previousSeam, x, y):
+    seamX = previousSeam[y][1]
     cost = 0
     for i in range(min(x, seamX), max(x, seamX)):
-        channelSum1 = 0
-        channelSum2 = 0
-        for j in range(0, currentFrame.shape[2]):
-            channelSum1 += currentFrame[y][i][j]
-            channelSum2 += currentFrame[y][i+1][j]
-        cost += abs(channelSum1 - channelSum2)
+        channels1 = np.linalg.norm(currentFrame[y][i])
+        channels2 = np.linalg.norm(currentFrame[y][i+1])
+        cost += abs(channels1 - channels2)
     return cost
 
-def compute_temporal_coherence_cost(currentFrame, previousSeam):
-    cost = []
-    for i in range(0, currentFrame.shape[0]):
-        cost.append([])
-        for j in range(0, currentFrame.shape[1]):
-            cost[i].append(compute_temporal_coherence_cost_pixel(currentFrame, previousSeam, j, i))
+def compute_temporal_coherence_cost_pixel2(currentFrame, previousSeam, x, y):
+    seamX = previousSeam[y][1]
+    cost = 0
+    row = currentFrame[y]
+    rowWithRemovedPixel = np.delete(row.copy(), seamX)
+    for i in range(0, x):
+        cost += np.linalg.norm(row[i] - rowWithRemovedPixel[i]) ** 2
+    for i in range(x+1, row.shape[0]):
+        cost += np.linalg.norm(row[i] - rowWithRemovedPixel[i-1]) ** 2
     return cost
+
+
+def compute_temporal_coherence_cost(currentFrame, previousSeam):
+    costMap1 = []
+    costMap2 = []
+    for i in range(0, currentFrame.shape[0]):
+        costMap1.append([])
+        costMap2.append([])
+        for j in range(0, currentFrame.shape[1]):
+            costMap1[i].append(compute_temporal_coherence_cost_pixel1(currentFrame, previousSeam, j, i))
+            costMap2[i].append(compute_temporal_coherence_cost_pixel2(currentFrame, previousSeam, j, i))
+    return (costMap1, costMap2)
 
 def compute_spatial_coherence_cost_pixel(row, rowAbove, x, y):
     # If border pixel
@@ -142,5 +155,9 @@ if __name__ == '__main__':
 
     video = read_video(args.video)
     saliency_map(video[0])
-
+    frame = np.random.randint(low=1, high=100, size=30)
+    frame = np.reshape(frame, (5, 6))
+    print(frame)
+    seam, energies = seam.carve_seams(frame)
+    costMaps = compute_temporal_coherence_cost(frame, seam[0])
     write_video(video, args.out)
