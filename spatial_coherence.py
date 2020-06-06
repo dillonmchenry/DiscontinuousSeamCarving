0c03aa6983fccf556d73a7d543fd4b0356dff025
dillonmchenry <dillon.mchenry7@gmail.com>
2020-06-06 14:29:36 -0400

    improved spatial

--- spatial_coherence.py
+++ spatial_coherence.py
@@ -3,49 +3,16 @@ import imageio
 import numpy as np
 
 
-def compute_spatial_coherence_cost_pixel(row, rowAbove, x, y, window):
-    horizontalCost = 0
-    verticalCost = 0
+def calculate_transition_cost(xa, x, row, rowAbove):
+    sum = 0
+    for j in range(xa, x):
+        gradient = abs(abs(row[j] - rowAbove[j]) - abs(row[j] - rowAbove[j + 1]))
+        if j < x:
+            sum += gradient
+        if j > xa:
+            sum += gradient
 
-    # Border Cases
-    if x == 0:
-        horizontalCost = abs(abs(row[x] - row[x + 1]) - abs(row[x + 1] - row[x + 2]))
-    elif x == row.shape[0] - 1:
-        horizontalCost = abs(abs(row[x] - row[x - 1]) - abs(row[x - 1] - row[x - 2]))
-    # Internal Pixel
-    else:
-        horizontalCost = abs(row[x - 1] - row[x]) + abs(row[x] - row[x + 1]) - abs(
-            row[x - 1] - row[x + 1])
-
-    # ----------------------Vertical Cost---------------------------------
-    sum1 = 0
-    sum2 = 0
-
-    for i in range(1, window):
-
-        # Moves to the left (if possible)
-        if x - i >= 0:
-            xa = x - i
-            for j in range(xa, x):
-                gradient = abs(abs(row[j] - rowAbove[j]) - abs(row[j] - rowAbove[j + 1]))
-                if j < x:
-                    sum1 += gradient
-                if j > xa:
-                    sum2 += gradient
-
-        # Moves to the right (if possible)
-        if x + i <= len(row) - 1:
-            xa = x + i
-            for j in range(xa, x):
-                gradient = abs(abs(row[j] - rowAbove[j]) - abs(row[j] - rowAbove[j + 1]))
-                if j < x:
-                    sum1 += gradient
-                if j > xa:
-                    sum2 += gradient
-
-    verticalCost = sum1 + sum2
-
-    return horizontalCost + verticalCost
+    return sum
 
 
 def compute_spatial_coherence_cost(frame, window):
@@ -54,9 +21,53 @@ def compute_spatial_coherence_cost(frame, window):
     gray_scale = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
     gray_scale = np.asarray(gray_scale, dtype=int)
 
-    for i in range(0, height):
-        for j in range(0, width):
-            cost_map[i][j] = compute_spatial_coherence_cost_pixel(gray_scale[i], gray_scale[i - 1], j, i, window)
+    for y in range(0, height):
+        gradients = []
+        for x in range(0, width):
+            row = gray_scale[y]
+            rowAbove = gray_scale[y - 1]
+
+            horizontalCost = 0
+            verticalCost = 0
+
+            # Border Cases
+            if x == 0:
+                horizontalCost = abs(abs(row[x] - row[x + 1]) - abs(row[x + 1] - row[x + 2]))
+            elif x == row.shape[0] - 1:
+                horizontalCost = abs(abs(row[x] - row[x - 1]) - abs(row[x - 1] - row[x - 2]))
+            # Internal Pixel
+            else:
+                horizontalCost = abs(row[x - 1] - row[x]) + abs(row[x] - row[x + 1]) - abs(
+                    row[x - 1] - row[x + 1])
+
+            # ----------------------Vertical Cost---------------------------------
+
+            left_bound = window
+            right_bound = window
+
+            # checks left boundary
+            if x < window:
+                left_bound -= window - x
+            # checks right boundary
+            elif x + window > len(row) - 1:
+                right_bound -= x + window - len(row)
+
+            if not gradients:
+                # calculates and sums transition for all pixels in window radius
+                for i in range(-left_bound, right_bound + 1):
+                    xa = x + i
+                    gradients.append(calculate_transition_cost(xa, x, row, rowAbove))
+
+            else:
+                # Checks for boundary cases
+                if left_bound >= window:
+                    gradients.pop()
+                if right_bound < window:
+                    gradients.append(calculate_transition_cost(right_bound, x, row, rowAbove))
+
+            verticalCost = sum(gradients)
+
+            cost_map[y][x] = verticalCost + horizontalCost
 
     return cost_map
 
@@ -66,4 +77,3 @@ if __name__ == '__main__':
     spatial_map = compute_spatial_coherence_cost(im, 10)
     spatial_map = spatial_map / np.max(spatial_map) * 255
     cv2.imwrite("spatial_demo.jpg", spatial_map)
-
