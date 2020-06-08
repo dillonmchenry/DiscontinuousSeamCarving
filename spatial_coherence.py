510bc50a303b583cbe4401e4328887e50c21eedc
dillonmchenry <dillon.mchenry7@gmail.com>
2020-06-07 22:29:58 -0400

    sweep sweep

--- spatial_coherence.py
+++ spatial_coherence.py
@@ -2,17 +2,17 @@ import cv2
 import imageio
 import numpy as np
 from numba import jit
-
 import disc_video_carving
 
 
 def calculate_transition_cost(xa, x, row, rowAbove):
     sum = 0
     for j in range(xa, x):
-        if j < x: # left side
+        if j < x:  # left side
             sum += abs(abs(row[j] - rowAbove[j]) - abs(row[j] - rowAbove[j + 1]))
-        if j > xa: # right side
+        if j > xa:  # right side
             sum += abs(abs(row[j] - rowAbove[j]) - abs(row[j] - rowAbove[j - 1]))
+
 
 def compute_spatial_coherence_cost_pixel(row, rowAbove, x, y, window):
     horizontalCost = 0
@@ -34,7 +34,7 @@ def compute_spatial_coherence_cost_pixel(row, rowAbove, x, y, window):
 
     # Moves to the left (if possible)
     xa = x - window
-    if (xa < 0):
+    if xa < 0:
         xa = 0
     for j in range(xa, x):
         gradient = abs(abs(row[j] - rowAbove[j]) - abs(row[j] - rowAbove[j + 1]))
@@ -45,7 +45,7 @@ def compute_spatial_coherence_cost_pixel(row, rowAbove, x, y, window):
 
     # Moves to the right (if possible)
     xa = x + window
-    if (xa >= row.shape[0]):
+    if xa >= row.shape[0]:
         xa = row.shape[0]
     for j in range(x, xa):
         gradient = abs(abs(row[j] - rowAbove[j]) - abs(row[j] - rowAbove[j - 1]))
@@ -58,77 +58,18 @@ def compute_spatial_coherence_cost_pixel(row, rowAbove, x, y, window):
 
     return horizontalCost + verticalCost
 
-def compute_spatial_coherence_cost2(frame, window):
-    height, width = frame.shape[:2]
-    cost_map = np.zeros((height, width))
-    gray_scale = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
-    gray_scale = np.asarray(gray_scale, dtype=int)
-
-    for y in range(0, height):
-        gradientsV = []
-        gradientsDR = []
-        gradientsDL = []
-        for x in range(0, width):
-            row = gray_scale[y]
-            rowAbove = gray_scale[y - 1]
-
-            horizontalCost = 0
-            verticalCost = 0
-
-            # Border Cases
-            if x == 0:
-                horizontalCost = abs(abs(row[x] - row[x + 1]) - abs(row[x + 1] - row[x + 2]))
-            elif x == row.shape[0] - 1:
-                horizontalCost = abs(abs(row[x] - row[x - 1]) - abs(row[x - 1] - row[x - 2]))
-            # Internal Pixel
-            else:
-                horizontalCost = abs(row[x - 1] - row[x]) + abs(row[x] - row[x + 1]) - abs(row[x - 1] - row[x + 1])
-
-            # ----------------------Vertical Cost---------------------------------
-            if y > 0:
-                if len(gradientsV) == 0:
-                    # calculates and sums transition for all pixels in window radius
-                    for i in range(window):
-                        gradientsV.append(abs(row[i] - rowAbove[i]))
-                        gradientsDL.append(abs(row[i + 1] - rowAbove[i]))
-                    gradientsV.append(abs(row[window] - rowAbove[window]))
-
-                leftBound = x if x < window else window
-                rightBound = window + leftBound if x + window < width else window + leftBound + (width - (x + window))
-                verticalCost = abs(sum(gradientsV[0:leftBound]) - sum(gradientsDR)) * 2 + abs(sum(gradientsV[leftBound:rightBound]) - sum(gradientsDL)) * 2
-                if x != 0:
-                    verticalCost -= gradientsV[0]
-                if x != width - 1:
-                    verticalCost -= gradientsV[len(gradientsV) - 1]
-
-                verticalCost = abs(verticalCost)
-
-                if x < width - 1:
-                    gradientsDR.append(abs(row[x] - rowAbove[x + 1]))
-                    gradientsDL.pop(0)
-                if x > window:
-                    gradientsV.pop(0)
-                    gradientsDR.pop(0)
-                if x + window < width:
-                    gradientsV.append(abs(row[x + window] - rowAbove[x + window]))
-                if x + window < width - 1:
-                    gradientsDL.append(abs(row[x + window + 1] - rowAbove[x + window]))
-
-            cost_map[y][x] = verticalCost + horizontalCost
-
-    return cost_map
 
 @jit(nopython=True)
 def compute_spatial_coherence_cost(frame, window):
-    #Precompute vertical gradients
+    # Precompute vertical gradients
     height, width = frame.shape[:2]
     costMap = np.zeros((height, width))
 
     verticalGradients = np.zeros((height, width))
-    DRGradients = np.zeros((height, width-1))
+    DRGradients = np.zeros((height, width - 1))
     for i in range(0, frame.shape[0]):
         row = frame[i]
-        if (i > 0):
+        if i > 0:
             rowAbove = frame[i - 1]
             verticalGradients[i] = np.absolute(row - rowAbove)
             DRGradients[i] = np.absolute(row[:-1] - rowAbove[1:])
@@ -141,14 +82,16 @@ def compute_spatial_coherence_cost(frame, window):
             else:
                 horizontalCost = np.absolute(row[j - 1] - row[j]) + np.absolute(row[j] - row[j + 1]) - np.absolute(
                     row[j - 1] - row[j + 1])
-            if (i == 0):
+            if i == 0:
                 costMap[i][j] = horizontalCost
                 continue
             leftBound = j - window if (j - window > 0) else 0
             rightBound = j + window if (j + window < row.shape[0]) else row.shape[0]
-            verticalCost = np.sum(verticalGradients[i-1][leftBound:rightBound]) + np.sum(DRGradients[i-1][leftBound:rightBound])
+            verticalCost = np.sum(verticalGradients[i - 1][leftBound:rightBound]) + np.sum(
+                DRGradients[i - 1][leftBound:rightBound])
             costMap[i][j] = verticalCost + horizontalCost
     return costMap
+
 
 if __name__ == '__main__':
     video = disc_video_carving.read_video("bowser_dunk_480.m4v")
@@ -162,4 +105,3 @@ if __name__ == '__main__':
     spatial_map = compute_spatial_coherence_cost(im, 10)
     spatial_map = spatial_map / np.max(spatial_map) * 255
     cv2.imwrite("spatial_demo.jpg", spatial_map)
-
